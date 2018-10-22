445757a5b25aa4639d1f8f5572073fad35ddd5fe
Felix Mannhardt <felix.mannhardt@sintef.no>
2018-10-23 00:12:15 +0200

    Fix failing test due to rlang deprecation warning

--- tests/testthat/test-issue-4-order.R
+++ tests/testthat/test-issue-4-order.R
@@ -3,5 +3,5 @@ context("test-issue-4-order.R")
 test_that("patients example works", {
   library(eventdataR)
   data(patients)
-  expect_silent(animate_process(patients))
+  expect_warning(animate_process(patients))
 })
