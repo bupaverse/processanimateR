42cd176435e05392fe3600322471769a5726c852
Felix Mannhardt <felix.mannhardt@sintef.no>
2020-02-19 14:34:48 +0100

    Add hammerjs library for better mobile panning/zooming, Enable pan/zoom control on screen

--- inst/htmlwidgets/lib/modules/animation_renderer_graphviz.js
+++ inst/htmlwidgets/lib/modules/animation_renderer_graphviz.js
@@ -132,7 +132,31 @@ function PARendererGraphviz(el) {
         svgPan.fit();
         svgPan.center();
       } else {
-        svgPan = svgPanZoom(svg, { dblClickZoomEnabled: false, preventEventsDefaults: true });
+        svgPan = svgPanZoom(svg, { dblClickZoomEnabled: false,
+                                   preventEventsDefaults: false,
+                                   controlIconsEnabled: true,
+                                   maxZoom: 20,
+                                  customEventsHandler: {
+                                    // Halt all touch events
+                                    haltEventListeners: ['touchstart', 'touchend', 'touchmove', 'touchleave', 'touchcancel']
+
+                                    // Init custom events handler
+                                  , init: function(options) {
+                                      // Init Hammer
+                                      this.hammer = Hammer(options.svgElement)
+
+                                      // Handle double tap
+                                      this.hammer.on('doubletap', function(ev){
+                                        options.instance.zoomIn()
+                                      })
+                                    }
+
+                                    // Destroy custom events handler
+                                  , destroy: function(){
+                                      this.hammer.destroy()
+                                    }
+                                  }
+        });
       }
     }
 
